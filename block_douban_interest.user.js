faf763569c554c718d97d3fbbb6f9029368432c9
Yuchen Wu <eaufavor@gmail.com>
2016-01-05 19:36:50 -0500

    Update block_douban_interest.user.js
    Fix matching rules

--- block_douban_interest.user.js
+++ block_douban_interest.user.js
@@ -2,7 +2,7 @@
 // @name         Block Douban Interest
 // @namespace   
 // @updateURL    https://raw.githubusercontent.com/eaufavor/Block-Douban-Interest/master/block_douban_interest.user.js
-// @version      0.1
+// @version      0.2
 // @description  block douban interest
 // @author       eaufavor
 // @match        http://*.douban.com/*
@@ -15,7 +15,7 @@
 
 var divs = document.getElementsByClassName('status-item');
 for (var i=0;i<divs.length; i++) {
-    if ( divs[i].getAttribute('data-action') == "0" ){
+    if ( (divs[i].getAttribute('data-object-kind') == "1015" && divs[i].getAttribute('data-target-type') != "rec")|| divs[i].getAttribute('data-object-kind') == "1026" || divs[i].getAttribute('data-object-kind') == "1012" ){
         divs[i].style.display = 'none';
     }
 }
